fb5855d0b63b9c104f6ffc54c9279c4e5129ec1c
kauntia <kauntia@amazon.com>
2020-05-04 00:56:43 -0700

    format

--- ContatinerWithMostWater.py
+++ ContatinerWithMostWater.py
@@ -3,14 +3,15 @@ def maxArea(height):
     Input will be an array which is the height of wall on each position: https://leetcode.com/problems/container-with-most-water/
     '''
     head = 0
-    tail = len(height)-1
+    tail = len(height) - 1
     area = 0
     while tail > head:
-        area = max(area, min(height[head], height[tail])*(tail-head))
+        area = max(area, min(height[head], height[tail]) * (tail - head))
         if height[head] > height[tail]:
             tail = tail - 1
         else:
             head = head + 1
     return area
 
-print(maxArea([1,2,1,2]))
+
+print(maxArea([1, 2, 1, 2]))
--- isValidSudoku.py
+++ isValidSudoku.py
@@ -2,10 +2,10 @@ import numpy as np
 
 
 def isValidSudoku(board):
-    '''
+    """
     Given an initial sudoku board with numbers as list of lists, find if it is valid or not
     Each internal list is one row of sudoku
-    '''
+    """
     board = np.array(board)
 
     def _listCheck(numList):
@@ -17,19 +17,19 @@ def isValidSudoku(board):
         return True
 
     for i in range(board.shape[0]):
-        if _listCheck(board[i].tolist()) == False:
+        if not _listCheck(board[i].tolist()):
             return False
             break
 
     for i in range(board.shape[0]):
-        if _listCheck(board[:, i].tolist()) == False:
+        if not _listCheck(board[:, i].tolist()):
             print("col fail", i)
             return False
             break
 
     for i in range(3):
         for j in range(3):
-            if _listCheck(board[i*3:i*3+3, j*3:j*3+3].flatten().tolist()) == False:
+            if not _listCheck(board[i * 3:i * 3 + 3, j * 3:j * 3 + 3].flatten().tolist()):
                 print("grid fail")
                 return False
                 break
